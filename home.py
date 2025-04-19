308dc168e7ac61c5fcb3ea6ffbe25784b2fe0aa4
Teresa Newman <tnewman9219@gmail.com>
2025-04-18 22:06:29 -0400

    init

--- home.py
+++ home.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 import sqlite3
 
+# 
 
 root = tk.Tk()
 root.title("Main")
@@ -44,22 +45,22 @@ def display_low_inventory():
     result_label.config(text=result_text)
 
 # Create and place the threshold input
-threshold_label = tk.Label(left_bottom, text="Enter Inventory Threshold:")
+threshold_label = tk.Label(left_top, text="Enter Inventory Threshold:")
 threshold_label.grid(row =8, column =0)
 
-threshold_entry = tk.Entry(left_bottom)
+threshold_entry = tk.Entry(left_top)
 threshold_entry.grid(row =9, column =0)
 
 
-result_label = tk.Label(left_bottom, text="")
+result_label = tk.Label(left_top, text="")
 result_label.grid(row =10, column =0)
 
 
 logo = tk.Label(left_top, text="Logo", width=20, height=2,font =('Helvetica', 18), pady=16)
 logo.grid(row =0, column =0)
 
-low_stock = tk.Button(left_bottom, text="Low Stock", width=20, height=2,font =('Helvetica', 18), bg = '#ffffff', command=display_low_inventory)
-low_stock.grid(row =1, column =0)
+low_stock = tk.Button(left_top, text="Low Stock", width=20, height=2,font =('Helvetica', 18), bg = '#ffffff', command=display_low_inventory)
+low_stock.grid(row =7, column =0)
 
 Aisle = tk.Button(left_top, text="Aisle", width=20, height=2,font =('Helvetica', 18), bg = '#ffffff')
 Aisle.grid(row =2, column =0)
@@ -73,8 +74,8 @@ Searcha.grid(row =4, column =0)
 Veiw_all = tk.Button(left_top, text="Veiw all", width=20, height=2,font =('Helvetica', 18), bg = '#ffffff')
 Veiw_all.grid(row =5, column =0)
 
-Log_out = tk.Button(left_top, text="Log out", width=20, height=2,font =('Helvetica', 18), bg = '#ffffff')
-Log_out.grid(row =6, column =0)
+Log_out = tk.Button(left_bottom, text="Log out", width=20, height=2, font =('Helvetica', 18), bg = '#ffffff')
+Log_out.grid(row =0, column =0)
 
 Inventory_label = tk.Label(Frame_right, text="Inventory",font =('Helvetica', 28), width=20, height=2, bg = '#ffffff')
 Inventory_label.grid(row =1, column =1)
